f10aafca7b7c36ea8f673657e9cbd572cc45cec7
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-23 10:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+// Get all the prices from the table
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Calculate the total price by summing up all the prices
+let totalPrice = 0;
+for (let i = 0; i < prices.length; i++) {
+  totalPrice += parseFloat(prices[i].innerText);
+}
+
+// Add the total price to the table
+const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotalCell.innerText = totalPrice.toFixed(2);
